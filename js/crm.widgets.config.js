39d5cdb460db0ccaa791d30a7aa8e085bb5e6028
czarza <czarzap@gmail.com>
2022-01-31 09:45:21 -0600

    js config editg

--- js/crm.widgets.config.js
+++ js/crm.widgets.config.js
@@ -3,6 +3,7 @@ const CrmWidgetsConfigModule = angular.module("prf.widgets.config", []);
 const EnvConfig = {
   api: "https://api.grupoblack.co/",
   streamer: "https://streamer.grupoblack.co/",
+  requestCache: "true",
 };
 
 CrmWidgetsConfigModule.constant("prfEnvConfig", EnvConfig);
